aea0d34beaf1e5cfe6aa6c47e2217b8b82f93b9e
kaleemramay <33531984+kaleemramay@users.noreply.github.com>
2019-03-31 20:25:20 -0400

    adding minesweeper v1

---
+++ src/minesweeper.js
@@ -0,0 +1,10 @@
+const blankLine = '  |   |  ';
+// console.log(blankLine)
+// console.log(blankLine)
+// console.log(blankLine)
+const guessLine  = '1 |   |  ';
+const bombLine  = '  | B |  ';
+
+console.log(guessLine)
+console.log(bombLine)
+console.log(blankLine)
