0ed0e1f46a1645673a8f22d65a1c288d91de29b6
kaleem <kaleem@tumblr.com>
2019-04-15 09:33:06 -0400

    renamed old minesweeper files to be versioned

---
+++ src/minesweeper_v2.js
@@ -0,0 +1,22 @@
+const printBoard = board => {
+    // Current Board
+    //console.log(`Current Board: ${board}`);
+    console.log("Current Board: ");
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+}
+
+let board = [];
+board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+]
+
+printBoard(board);
+
+board[0][1] = '1'
+board[2][2] = 'B'
+
+printBoard(board);
